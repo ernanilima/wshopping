60cc8c1a706ce7285ed46e4ca17b8af99c0d2b85
ernanilima <ernani.tecc@gmail.com>
2023-04-30 19:21:43 -0300

    WSAP-1
    Obter uri para rota da api

--- src/app/api/consume/consume.component.ts
+++ src/app/api/consume/consume.component.ts
@@ -1,6 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   template: '<p>Acessar a API</p>',
 })
-export class ConsumeComponent {}
+export class ConsumeComponent implements OnInit {
+  constructor(private route: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const uri: string = this.route.url.split(this._path!)[1];
+    console.log('url completa:', uri);
+  }
+
+  private get _path() {
+    return this.activatedRoute.snapshot.root.firstChild?.routeConfig?.path;
+  }
+}
