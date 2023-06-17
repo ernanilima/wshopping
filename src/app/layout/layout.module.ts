184f8d801bce7b610d8ab414181aab037ba0d6dd
ernanilima <ernani.tecc@gmail.com>
2023-06-17 19:11:46 -0300

    Footer

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AppFooterComponent } from './footer/app.footer.component';
 import { AppMenuComponent } from './menu/app.menu.component';
 import { ItemComponent } from './menu/item/item.component';
 import { AppSidebarComponent } from './sidebar/app.sidebar.component';
@@ -12,8 +13,9 @@ import { AppTopbarComponent } from './topbar/app.topbar.component';
     AppSidebarComponent,
     AppMenuComponent,
     ItemComponent,
+    AppFooterComponent,
   ],
-  exports: [AppTopbarComponent, AppSidebarComponent],
+  exports: [AppTopbarComponent, AppSidebarComponent, AppFooterComponent],
   imports: [CommonModule, RouterModule],
 })
 export class LayoutModule {}
