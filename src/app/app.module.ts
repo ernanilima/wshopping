bbaf0dd568dbce550180b38f834d68224262a5e5
ernanilima <ernani.tecc@gmail.com>
2023-06-24 13:04:01 -0300

    Atualizado service com pipe para descricao e data da marca

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { DatePipe, TitleCasePipe } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -14,6 +15,7 @@ import { AppComponent } from './app.component';
     HttpClientModule,
     BrowserAnimationsModule,
   ],
+  providers: [TitleCasePipe, DatePipe],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/pages/brand/service/brand.service.ts
+++ src/app/pages/brand/service/brand.service.ts
@@ -1,19 +1,38 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, take } from 'rxjs';
+import { Observable, map, take } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { BrandDto } from '../model/brand.dto';
 import { Response } from 'src/app/shared/params/response';
+import { DatePipe, TitleCasePipe } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BrandService {
-  constructor(private _http: HttpClient) {}
+  constructor(
+    private _http: HttpClient,
+    private _titlecase: TitleCasePipe,
+    private _datePipe: DatePipe
+  ) {}
 
   public findAll(): Observable<Response<BrandDto[]>> {
     return this._http
       .get<Response<BrandDto[]>>(`${environment.baseUrl}/v1/marca`)
+      .pipe(
+        map((resp: Response<BrandDto[]>) => ({
+          ...resp,
+          content: resp.content.map((dto) => this.filter(dto)),
+        }))
+      )
       .pipe(take(1));
+  }
+
+  private filter(dto: BrandDto): BrandDto {
+    return {
+      code: dto.code,
+      description: this._titlecase.transform(dto.description),
+      created_at: this._datePipe.transform(dto.created_at, 'dd/MM/yyyy HH:mm'),
+    };
   }
 }
