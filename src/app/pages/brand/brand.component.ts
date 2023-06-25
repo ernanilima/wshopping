c142be3ca08cd050bd873d40f2f1968e83174990
ernanilima <ernani.tecc@gmail.com>
2023-06-25 10:41:14 -0300

    Paginacao lazy para marcas

--- src/app/pages/brand/brand.component.ts
+++ src/app/pages/brand/brand.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TableLazyLoadEvent } from 'primeng/table';
 import { Response } from 'src/app/shared/params/response';
 import { BrandDto, brandColumns } from './model/brand.dto';
 import { BrandService } from './service/brand.service';
@@ -8,14 +9,23 @@ import { BrandService } from './service/brand.service';
   templateUrl: './brand.component.html',
 })
 export class BrandComponent implements OnInit {
+  public loading = true;
   public columns = brandColumns;
   public brands: Response<BrandDto[]>;
 
   constructor(private _service: BrandService) {}
 
   public ngOnInit(): void {
+    this.findBrands();
+  }
+
+  public findBrands(event: TableLazyLoadEvent = null): void {
+    console.log(event);
+
+    this.loading = true;
     this._service.findAll().subscribe((brands: Response<BrandDto[]>) => {
       this.brands = brands;
+      this.loading = false;
     });
   }
 }
