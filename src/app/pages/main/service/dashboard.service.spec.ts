96e0b908218c9ef8b4b69d93c98982d250024f6a
ernanilima <ernani.tecc@gmail.com>
2023-10-21 13:27:41 -0300

    Cobertura de testes para pages/main/service

--- src/app/pages/main/service/dashboard.service.spec.ts
+++ src/app/pages/main/service/dashboard.service.spec.ts
@@ -1,18 +1,72 @@
-import { HttpClientModule } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { environment } from 'src/environments/environment';
 import { DashboardService } from './dashboard.service';
 
 describe('DashboardService', () => {
   let service: DashboardService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule],
+      imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(DashboardService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('findTotalBrands', () => {
+    it('should return the total when calling the findTotalBrands method', () => {
+      const expected = 31;
+
+      service.findTotalBrands().subscribe((result) => {
+        expect(result).toEqual(expected);
+      });
+
+      const req = httpMock.expectOne(
+        `${environment.baseUrl}/v1/dashboard/total-marcas`
+      );
+      expect(req.request.method).toEqual('GET');
+      req.flush(expected);
+    });
+  });
+
+  describe('findTotalProducts', () => {
+    it('should return the total when calling the findTotalProducts method', () => {
+      const expected = 32;
+
+      service.findTotalProducts().subscribe((result) => {
+        expect(result).toEqual(expected);
+      });
+
+      const req = httpMock.expectOne(
+        `${environment.baseUrl}/v1/dashboard/total-produtos`
+      );
+      expect(req.request.method).toEqual('GET');
+      req.flush(expected);
+    });
+  });
+
+  describe('findTotalProductsNotFound', () => {
+    it('should return the total when calling the findTotalProductsNotFound method', () => {
+      const expected = 33;
+
+      service.findTotalProductsNotFound().subscribe((result) => {
+        expect(result).toEqual(expected);
+      });
+
+      const req = httpMock.expectOne(
+        `${environment.baseUrl}/v1/dashboard/total-produtos-nao-encontrados`
+      );
+      expect(req.request.method).toEqual('GET');
+      req.flush(expected);
+    });
   });
 });
