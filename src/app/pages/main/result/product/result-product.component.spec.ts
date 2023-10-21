6bd4f7b237db5a737ac9954eeb66f6774136b00d
ernanilima <ernani.tecc@gmail.com>
2023-10-20 22:13:01 -0300

    Cobertura de testes para pages/modal/product

--- src/app/pages/main/result/product/result-product.component.spec.ts
+++ src/app/pages/main/result/product/result-product.component.spec.ts
@@ -1,11 +1,25 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppModule } from 'src/app/app.module';
 import { PagesModule } from 'src/app/pages/pages.module';
+import { ProductDto } from 'src/app/pages/product/model/product.dto';
 import { ResultProductComponent } from './result-product.component';
+
+const product = {
+  id: '84a34074-325e-4ee6-9066-b40b4b1e6948',
+  barcode: '7891020300000',
+  description: 'Test Descript Product',
+  brand: {
+    description: 'Test Descript Brand',
+  },
+  created_at: '2023-10-10T20:55:44.245Z',
+} as ProductDto;
 
 describe('ResultProductComponent', () => {
   let component: ResultProductComponent;
+  let debugElement: DebugElement;
   let fixture: ComponentFixture<ResultProductComponent>;
 
   beforeEach(waitForAsync(() => {
@@ -17,24 +31,53 @@ describe('ResultProductComponent', () => {
       .then(() => {
         fixture = TestBed.createComponent(ResultProductComponent);
         component = fixture.componentInstance;
-        component.product = {
-          id: '95de4a85-63ab-483f-a743-18490aea90b8',
-          barcode: '789102030',
-          description: 'Produto 102030',
-          brand: {
-            id: '61b832c7-acea-4924-ba99-d3a54b4ac2a3',
-            code: 101,
-            description: 'Marca 101',
-            total_products: 2,
-            created_at: '10/10/2023 20:30',
-          },
-          created_at: '11/11/2023 21:31',
-        };
+        debugElement = fixture.debugElement;
+        component.product = product;
         fixture.detectChanges();
       });
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('html', () => {
+    it('should contain the existing barcode on the product', () => {
+      const el = debugElement.query(By.css('#barcode'));
+      const codigoBarras = el.nativeElement;
+
+      const barcode = '7891020300000';
+
+      expect(component.product.barcode).toEqual(barcode);
+      expect(codigoBarras.value).toEqual(barcode);
+    });
+
+    it('should contain the existing productDescription on the product', () => {
+      const el = debugElement.query(By.css('#productDescription'));
+      const descricaoProduto = el.nativeElement;
+
+      const productDescription = 'Test Descript Product';
+
+      expect(component.product.description).toEqual(productDescription);
+      expect(descricaoProduto.value).toEqual(productDescription);
+    });
+
+    it('should contain the existing brandDescription on the product', () => {
+      const el = debugElement.query(By.css('#brandDescription'));
+      const descricaoMarca = el.nativeElement;
+
+      const brandDescription = 'Test Descript Brand';
+
+      expect(component.product.brand.description).toEqual(brandDescription);
+      expect(descricaoMarca.value).toEqual(brandDescription);
+    });
+
+    it('should contain the existing productCreatedAt on the product', () => {
+      const el = debugElement.query(By.css('#productCreatedAt'));
+      const produtoCriadoEm = el.nativeElement;
+
+      expect(component.product.created_at).toEqual('2023-10-10T20:55:44.245Z');
+      expect(produtoCriadoEm.value).toEqual('10/10/2023');
+    });
   });
 });
